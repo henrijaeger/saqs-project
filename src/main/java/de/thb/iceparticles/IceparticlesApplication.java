785924536419da7aa51984fe05df2feb39d1c8f6
Nuskel <blaszczn@th-brandenburg.de>
2022-11-21 13:40:19 +0100

    Setup Spring

---
+++ src/main/java/de/thb/iceparticles/IceparticlesApplication.java
@@ -0,0 +1,13 @@
+package de.thb.iceparticles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceparticlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceparticlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/thb/iceparticles/IceparticlesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thb.iceparticles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceparticlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
